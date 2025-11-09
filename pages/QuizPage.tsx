e8c0f26e4192a0f7903f11cdc740426b62ae4121
PYTHAGON2 <164600673+PYTHAGON2@users.noreply.github.com>
2025-11-09 23:00:00 +0100

    version 1

--- pages/QuizPage.tsx
+++ pages/QuizPage.tsx
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
+//THIS IS QUIZ PAGE.TSX
 interface QuizPageProps {
   quiz: Quiz;
   user: User;
